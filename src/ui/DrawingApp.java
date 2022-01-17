61005e08f9d3ac38035dafc54db39396de031d12
jonathan <jonathan.guerin@telecom-paris.fr>
2022-01-17 15:11:26 +0100

    Debut interfaces graphiques

---
+++ src/ui/DrawingApp.java
@@ -0,0 +1,13 @@
+package ui;
+
+import javax.swing.*;
+
+public class DrawingApp extends JFrame{
+	public DrawingApp() {
+		super("Drawing Application");
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		pack();
+		setVisible(true);
+	}
+}
