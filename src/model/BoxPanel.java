c019f68f0846c70591f3623cce172935754d7fa5
jonathan <jonathan.guerin@telecom-paris.fr>
2022-02-20 17:10:11 +0100

    ajout du modele

--- src/model/BoxPanel.java
+++ src/model/BoxPanel.java
@@ -4,18 +4,99 @@ import java.awt.*;
 import javax.swing.*;
 
 import dijkstra.VertexInterface;
+import maze.*;
 import ui.MazeApp;
 
-public class BoxPanel extends JButton{
-	private final Color color;
+public class BoxPanel extends JPanel{
+	private Color color;
 	private VertexInterface box;
 	private final MazeApp mazeApp;
-	
 	
 	public BoxPanel(MazeApp mazeApp, VertexInterface box) {
 		this.color = Color.WHITE;
 		this.mazeApp = mazeApp;
 		this.box = box;
+		
+		setPreferredSize(new Dimension(50,50));
+		setBackground(this.color);
 	}
 	
+	@Override
+	protected void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		
+		int w = getWidth();
+		int h = getHeight();
+		
+		
+		if(this.box instanceof EBox) {
+			g.setColor(Color.GREEN);
+			g.fillRect(0,0,w,h);
+			g.setColor(java.awt.Color.black);
+			g.drawRect(0, 0, w, h);
+		}else if(this.box instanceof WBox) {
+			g.setColor(Color.RED);
+			g.fillRect(0,0,w,h);
+			g.setColor(java.awt.Color.black);
+			g.drawRect(0, 0, w, h);
+		}else if(this.box instanceof DBox) {
+			g.setColor(Color.YELLOW);
+			g.fillRect(0,0,w,h);
+			g.setColor(java.awt.Color.black);
+			g.drawRect(0, 0, w, h);
+		}else if(this.box instanceof ABox) {
+			g.setColor(Color.BLUE);
+			g.fillRect(0,0,w,h);
+			g.setColor(java.awt.Color.black);
+			g.drawRect(0, 0, w, h);
+		}
+		
+		
+		/*
+		//IHM2_p14
+		this.color = this.mazeApp.getModel().getSelectedMode();
+		
+		g.drawString(this.selectedMode,0,10);
+		g.setColor(this.mazeApp.getModel().getSelectedColor());
+		g.fillRoundRect(4, 4, w-8, h-8, 15,15);
+		*/
+		
+		
+		/*
+		private final static float[] dash = { 4.0f } ;
+		   
+		private final static BasicStroke usualStroke  ;
+		private final static BasicStroke largeStroke ;
+		private final static BasicStroke dashStroke  ;
+		   
+		static { // Static initialization of drawing modes
+		   usualStroke  = new BasicStroke() ;
+		   largeStroke  = new BasicStroke(3.0f, BasicStroke.CAP_ROUND, BasicStroke.JOIN_MITER) ;
+		   dashStroke   = new BasicStroke(1.0f, BasicStroke.CAP_ROUND,
+		                                  BasicStroke.JOIN_MITER, 10.0f, dash, 0.0f) ;	   
+		}
+		   
+		public final void paint(Graphics g, boolean selected, boolean current){
+		   Graphics2D g2 = (Graphics2D)g ;
+		   g2.setColor(color);
+			   
+		   if (current) {   
+		      g2.setStroke(dashStroke) ;  // Dashed lines mode
+		      g2.draw(this) ;
+		      g2.setStroke(usualStroke) ; // Normal mode
+		   } else if (selected) {
+		      g2.setStroke(largeStroke) ; // Large lines mode
+		      g2.draw(this) ;
+		      g2.setStroke(usualStroke) ; // Normal mode
+		   } else {
+		      g2.draw(this) ;
+		   }
+		}
+		*/
+		
+	}
+	
+	public void setColor(Color color) {
+		this.color = color;
+	}
 }
--- src/model/MazeAppModel.java
+++ src/model/MazeAppModel.java
@@ -1,18 +1,27 @@
 package model;
 
 import java.awt.*;
+import java.util.ArrayList;
+import javax.swing.event.*;
 
 import dijkstra.GraphInterface;
 import dijkstra.VertexInterface;
 import maze.EBox;
 import maze.Maze;
+import maze.WBox;
+import ui.MazeApp;
 
 public final class MazeAppModel {
 	private GraphInterface maze;
+	private MazeApp mazeApp;
 	private int height = 5;
 	private int width = 12;
+	private String selectedMode = "W";
+	private Color selectedColor;
+	private ArrayList<ChangeListener> listeners = new ArrayList<ChangeListener>() ;
 	
-	public MazeAppModel() {
+	public MazeAppModel(MazeApp mazeApp) {
+		this.mazeApp = mazeApp;
 		VertexInterface[][] boxes = new VertexInterface[height][width];
 		this.maze = new Maze(boxes,height,width);
 		for(int i=0;i < height;i++) {
@@ -20,6 +29,23 @@ public final class MazeAppModel {
 				boxes[i][j] = new EBox(maze,i,j);
 			}
 		}
+		this.setSelectedColor();
+	}
+	
+	public void setSelectedColor() {
+		if(selectedMode == "W") {
+			this.selectedColor = Color.RED; 
+		}else if(selectedMode == "E") {
+			this.selectedColor = Color.GREEN;
+		}else if(selectedMode == "D") {
+			this.selectedColor = Color.YELLOW;
+		}else if(selectedMode == "A") {
+			this.selectedColor = Color.ORANGE;
+		}
+	}
+	
+	public Color getSelectedColor() {
+		return this.selectedColor;
 	}
 	
 	public int getWidth() {
@@ -34,4 +60,38 @@ public final class MazeAppModel {
 		return this.maze;
 	}
 	
+	public String getSelectedMode() {
+		return this.selectedMode;
+	}
+	
+	public void setSelectedMode(String selectedMode) {
+		this.selectedMode = selectedMode;
+		this.stateChanges();
+	}
+	
+	public final void paintBoxes(Graphics g) {
+		
+		for(int i=0; i < height ; i++) {
+			for(int j = 0; j < width ; j++) {
+				//this.mazeApp.getMazePanel().getBoxesPanel()[i][j].paintNormal();
+			}
+		}
+		this.stateChanges();
+	}
+	
+	public void addObserver(ChangeListener listener) {
+		listeners.add(listener) ;
+	}
+	
+	public void stateChanges() {
+        ChangeEvent evt = new ChangeEvent(this) ;
+        for (ChangeListener listener : listeners) {
+        	listener.stateChanged(evt);
+        }
+    }
+	
+	public final void setColorBox(int i , int j) {	
+		
+		this.stateChanges();
+	}
 }
